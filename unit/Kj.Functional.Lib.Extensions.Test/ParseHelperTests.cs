1aaff8353ed5bda90868f339c96b4b3fedffe3a2
Krzysztof Jablonski <kj@something.com>
2022-01-14 20:50:27 +0100

    added some attributes; added TryParseEnum

--- unit/Kj.Functional.Lib.Extensions.Test/ParseHelperTests.cs
+++ unit/Kj.Functional.Lib.Extensions.Test/ParseHelperTests.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using FluentAssertions;
+using Kj.Functional.Lib.Core;
 using Kj.Functional.Lib.Extensions.Parse;
 using NUnit.Framework;
 
@@ -23,5 +26,25 @@ public class ParseHelperTests
 	public void Parse_Int_Fails(string input)
 	{
 		input.TryParseInt().Match(r => true, () => false).Should().Be(false);
+	}
+
+	public enum TestEnum
+	{
+		One=1,
+		Two,
+		Three
+	}
+	
+	[TestCase("One", ExpectedResult = TestEnum.One)]
+	[TestCase("two", ExpectedResult = TestEnum.Two)]
+	[TestCase("tHREE", ExpectedResult = TestEnum.Three)]
+	public TestEnum ParseEnum_Success(string input)
+	{
+		var parsed = input.TryParseEnum<TestEnum>(true);
+		return parsed
+				.Do(v => { }, Assert.Fail)
+				.Match(v => v,
+					() => (TestEnum)0)
+			;
 	}
 }
